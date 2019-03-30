8561f533a52b68856edb6d6438fb0718b5cac12b
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2019-03-30 14:39:22 +0100

    Updates

---
+++ precache-manifest.58248bfb90ea231ab3e36b5d7d524d8e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7f32122b59ac7aca3feb",
+    "url": "/static/js/main.2ef54027.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "c0a97ff6d34487c0ae68c4fdebdb0fb1",
+    "url": "/index.html"
+  }
+];
